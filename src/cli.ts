f02d53de90e61b49417cb64f3b732d3568679ff1
jellychoco <ma88082@gmail.com>
2025-02-24 23:58:27 -0800

    Split web vs node

---
+++ src/cli.ts
@@ -0,0 +1,4 @@
+export { TranslationManager } from './manager/translation-manager';
+export { KeyManager } from './manager/key-manager';
+export { ExclusionManager } from './manager/exclusion-manager';
+export type { I18nConfig } from './types';
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 // 코어 기능 export
 export { i18n } from './core/i18n';
-export type { I18nConfig } from './types';
-
-// CLI 및 관리 도구 export
-export { TranslationManager } from './manager/translation-manager';
-export { KeyManager } from './manager/key-manager';
-export { ExclusionManager } from './manager/exclusion-manager';
